b0490e7d1cb6c61a8a693345b2b0856f50145e36
anya <132786918+stillanya@users.noreply.github.com>
2024-04-03 19:36:14 +0600

    Update PostsPage.js

--- src/PostsPage.js
+++ src/PostsPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchPosts } from './ps';
+import { fetchPosts } from './PostsSlice';
 
 const PostsPage = () => {
     const dispatch = useDispatch();
