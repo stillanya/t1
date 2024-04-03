dac589e67f166e3d1dd37e0a66a3d6a1cd934eaf
anya <132786918+stillanya@users.noreply.github.com>
2024-04-03 19:49:26 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import PostsPage from './PostsPage';
+import PostPage from './postPage';
 
 function App() {
   return (
